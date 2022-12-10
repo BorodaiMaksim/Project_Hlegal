62fc4f43eeeefef99e5cc7c7f5b6fa62d6ade433
Borodai Maksim <borodaymax20@gmail.com>
2022-12-10 17:11:15 +0100

    Added some configuration in Gruntfile.js and compiled scss files in css

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,4 +24,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('default', ['watch']);
+    grunt.loadNpmTasks('grunt-browserify');
 }
